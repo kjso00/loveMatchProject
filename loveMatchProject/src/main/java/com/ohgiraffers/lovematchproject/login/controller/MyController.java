38d5d077a58be76fd82088f7a9ef0609949d6503
dansun2 <dansunmusik7@gmail.com>
2024-07-30 10:58:04 +0900

    feat: Add Google and Naver login

---
+++ loveMatchProject/src/main/java/com/ohgiraffers/lovematchproject/login/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.lovematchproject.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class MyController {
+
+    @GetMapping("/my")
+    public String myPage() {
+        return "my";
+    }
+}
