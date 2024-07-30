505c6e846ec65e624de229eb5fbd662fdf8bf65b
phygloria <phygloria@gmail.com>
2024-07-30 23:24:15 +0900

    goingplease

--- loveMatchProject/src/main/java/com/ohgiraffers/lovematchproject/match/matchrepository/MatchRepository.java
+++ loveMatchProject/src/main/java/com/ohgiraffers/lovematchproject/match/matchrepository/MatchRepository.java
@@ -5,5 +5,5 @@ import com.ohgiraffers.lovematchproject.match.matchmodel.entity.MatchEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MatchRepository extends JpaRepository<MatchEntity, Long>{
-
+    MatchEntity findById(long id);
 }
