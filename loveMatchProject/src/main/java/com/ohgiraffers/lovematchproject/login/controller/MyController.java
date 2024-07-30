4fa0fe64e34c34db62d37b96f049b139b05b1aab
dansun2 <dansunmusik7@gmail.com>
2024-07-31 01:43:18 +0900

    feat: Add logout

--- loveMatchProject/src/main/java/com/ohgiraffers/lovematchproject/login/controller/MyController.java
+++ loveMatchProject/src/main/java/com/ohgiraffers/lovematchproject/login/controller/MyController.java
@@ -2,12 +2,13 @@ package com.ohgiraffers.lovematchproject.login.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@RequestMapping("/my")
 public class MyController {
 
-    @GetMapping("/my")
+    @GetMapping("/")
     public String myPage() {
         return "login/my";
     }
