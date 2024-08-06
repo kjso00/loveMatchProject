02aa36aafe4a243841c2efaf811b6e4663efb261
ju <phygloria@gmail.com>
2024-08-06 17:17:09 +0900

    feat : 주소 점수추가 api fix : 에러수정 refactor : 중복코드정리 build : kakakoMapApi

--- src/main/java/com/ohgiraffers/lovematchproject/match/matchcontroller/MatchController.java
+++ src/main/java/com/ohgiraffers/lovematchproject/match/matchcontroller/MatchController.java
@@ -3,36 +3,51 @@ package com.ohgiraffers.lovematchproject.match.matchcontroller;
 import com.ohgiraffers.lovematchproject.match.matchmodel.dto.MatchDTO;
 import com.ohgiraffers.lovematchproject.match.matchservice.MatchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.ModelAndView;
 
 
 import java.util.List;
 
-@Controller
+@RestController
 public class MatchController {
 
-    @Autowired
     private final MatchService matchService;
 
+    @Autowired
     public MatchController(MatchService matchService) {
         this.matchService = matchService;
     }
 
+    @GetMapping("/match/userForm")
+    public String matchUserForm(Model model) {
+        model.addAttribute("match", new MatchDTO());
+        return "match/matchUserForm";
+    }
+
     @GetMapping("/match/matchProfiles")
-    public String getMatches(Model model) {
-        long loginUserId = 1; // 현재 사용자의 ID를 하드코딩
+    public ModelAndView getMatches(ModelAndView mv) {
+
+        long loginUserId = 6; // 현재 사용자의 ID를 하드코딩
         MatchDTO loginUser = matchService.getLoginUser(loginUserId);
         List<MatchDTO> targetGender = matchService.getFilteringGender(loginUserId);
         List<MatchDTO> matchResults = matchService.calculatematchScores(loginUserId);
 
-        model.addAttribute("loginUser", loginUser);
-        model.addAttribute("filterGender", targetGender);
-        model.addAttribute("matchResults", matchResults);
-        return "match/matchProfiles";
+        mv.addObject("loginUser", loginUser);
+        mv.addObject("filterGender", targetGender);
+        mv.addObject("matchResults", matchResults);
+
+        mv.setViewName("match/matchProfiles");
+
+        return mv;
 
 //        long loginUser = 1; //현재 사용자의 ID를 하드코딩 -> 로그인사용자로 변경필요
     }
+
+
+
 
 }
