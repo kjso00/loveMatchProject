27b829df3142580086757000a2465cca0c4c1928
ju <phygloria@gmail.com>
2024-08-13 12:56:28 +0900

    refactor : list, Home controller,MainController

--- src/main/java/com/ohgiraffers/lovematchproject/profile/controller/HomeController.java
+++ src/main/java/com/ohgiraffers/lovematchproject/profile/controller/HomeController.java
@@ -1,14 +1,27 @@
 package com.ohgiraffers.lovematchproject.profile.controller;
 
+import com.ohgiraffers.lovematchproject.profile.model.dto.ProfileDTO;
+import com.ohgiraffers.lovematchproject.profile.service.ProfileService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 
 @Controller
 public class HomeController {
 
+    private final ProfileService profileService;
+    public HomeController(ProfileService profileService) {
+        this.profileService = profileService;
+    }
+
     @GetMapping("/home")
-    public String Home(){
-        return "profile/home";
+    public String Home(Model model){
+        List<ProfileDTO> profileDTOList = profileService.findAll();
+        model.addAttribute("profileDTOList", profileDTOList);
+        return "profile/list";
     }
 
 }
